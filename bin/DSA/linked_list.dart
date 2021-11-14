c6e000939c6ccecb30c41e85cd05f8686658f5eb
aninarafath6 <arafathanin6@gmail.com>
2021-11-14 13:45:11 +0530

    linkedList get first element and last elemt

--- bin/DSA/linked_list.dart
+++ bin/DSA/linked_list.dart
@@ -1,6 +1,7 @@
 class LinkedList {
-  Node? head;
-  Node? tail;
+  // head and tail initially null
+  Node? _head;
+  Node? _tail;
 
   // add value into linked list
   void add(int data) {
@@ -8,27 +9,90 @@ class LinkedList {
     Node newNode = Node(data);
 
     // checking linked list is already exist
-    if (head == null) {
+    if (_head == null) {
       // if lined list don't exist set node as head and tail
-      head = newNode;
+      _head = newNode;
     } else {
       // if linked list is already exist set node as tail.next
-      tail!.next = newNode;
+      _tail!.next = newNode;
     }
     // set new node as tail even linked list is don't exist
-    tail = newNode;
+    _tail = newNode;
   }
 
-// display all values
+  // add value at next of that element
+  void addAt(int where, int data) {
+    // create a newNode with given data
+    Node newNode = Node(data);
+    // create temporary node for go through all elements (for checking where data is contains this list)
+    // temp node is default its value is head
+    Node? temp = _head;
+
+    // loop will work when temp not null and temp.data equals where it means
+    //(loop will stop when temp equals null (means when temp on tail) or temp.data and where will same it means we found  the where value in this list)
+    while (temp != null && temp.data != where) {
+      // setting temp to temp.next its means temp will go through all values when loop will call it will brake when we found where value in this linked list or
+      // temp will tail means temp will be null
+      temp = temp.next;
+    }
+    // checking temp == null it means temp on tail and we not found the where value in this linked list it means the where value not found in this list
+    // so we can stop this function
+    if (temp == null) {
+      // handling situation
+      print('$where is not contains this list');
+    } else {
+      // here we have where value contains in list
+      // we are setting newNode' next = temp node's next
+      newNode.next = temp.next;
+      // temp nod's next = newNode
+      temp.next = newNode;
+    }
+  }
+
+  // add value on last element of this linked list
+  void addLast(int data) {
+    // create a node with given data
+    Node newNode = Node(data);
+    // checking if head  equals null its means linked list is not created so we can create a liked list
+    // so linked list's first node is head
+    if (_head == null) {
+      // we assigned newNode to head when  linked list is not created
+      _head = newNode;
+    } else {
+      // when linked list is already exist so we are assigning the new node to tail.next
+      _tail!.next = newNode;
+    }
+    // and we are changing tail to new node
+    // even the linked list is not cerated so this case tail and head is same
+    _tail = newNode;
+  }
+
+  // add value on first element of the LinkedList
+  void addFirst(int data) {
+    // crate  a new node
+    Node _newNode = Node(data);
+    // checking the head is null then create assign the new nod as head and tail
+    if (_head == null) {
+      _head = _newNode;
+      _tail = _newNode;
+    } else {
+      // inserting value as a first element in this list
+      _newNode.next = _head;
+      _head = _newNode;
+    }
+  }
+
+  // display all values
   void display() {
     // made a new temporary node for go through all values
-    Node? temp = head;
+    Node? temp = _head;
     // check if temp null then print empty
     if (temp == null) {
       print('Empty');
     } else {
       //else temp not null then go through all values in this linked list
-      // temp not null then loop will continue other wise when temp == null (means temp in last element means temp now in tail ) then loop will brake 
+      // temp not null then loop will continue other wise when temp == null
+      //(means temp in last element means temp now in tail ) then loop will brake
       while (temp != null) {
         // display current val temp node value
         print(temp.data);
@@ -38,13 +102,14 @@ class LinkedList {
     }
   }
 
+// get first element
 
+  int? get first => _head?.data;
+  int? get last =>_tail?.data;
   
 }
 
-
-
-// node class 
+// node class
 class Node {
   final int data;
   Node? next;
--- bin/coding_challenge.dart
+++ bin/coding_challenge.dart
@@ -3,10 +3,10 @@
 
 // import 'day_4.dart';
 import 'DSA/linked_list.dart';
-import 'day_5.dart';
-import 'day_6.dart';
-import 'exercise/ex_2.dart';
-import 'dart:math';
+// import 'day_5.dart';
+// import 'day_6.dart';
+// import 'exercise/ex_2.dart';
+// import 'dart:math';
 
 void main(List<String> arguments) {
   // day_1(100);
@@ -24,6 +24,15 @@ void main(List<String> arguments) {
   list.display();
   list.add(2);
   list.add(3);
-  list.add(4);
+  // list.add(4);
+  // list.addAt(2, 1);
+  // list.addFirst(1000);
+  // list.addFirst(2000);
+
   list.display();
+
+  print('getters');
+  print(list.first);
+  print(list.last);
+
 }
